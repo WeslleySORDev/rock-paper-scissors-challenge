c40d0e445f87da632999a1edb81fe445b8437b25
WeslleySORDev <weslleysordev@gmail.com>
2025-03-14 19:24:42 -0300

    feat: add react, react-router, vite, and types

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <>
+      <main>
+        Score Rules You Picked The House Picked You Win You Lose Play Again
+      </main>
+      <footer className="">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </>
+  );
+}
