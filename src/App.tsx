24e7fd27a678f00f9b560324d54cfe1b190c0e4d
WeslleySORDev <weslleysordev@gmail.com>
2025-03-15 10:53:01 -0300

    feat: add all possible moves constant to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+const POSSIBLE_MOVES = [
+  {
+    name: "paper",
+    image_path: "/assets/images/icon-paper.svg",
+    wins: ["rock", "spock"],
+    loses: ["scissors", "lizard"]
+  },
+  {
+    name: "scissors",
+    image_path: "/assets/images/icon-scissors.svg",
+    wins: ["paper", "lizard"],
+    loses: ["spock", "rock"]
+  },
+  {
+    name: "rock",
+    image_path: "/assets/images/icon-rock.svg",
+    wins: ["scissors", "lizard"],
+    loses: ["spock", "paper"]
+  },
+  {
+    name: "spock",
+    image_path: "/assets/images/icon-spock.svg",
+    wins: ["scissors", "rock"],
+    loses: ["lizard", "paper"]
+  },
+  {
+    name: "lizard",
+    image_path: "/assets/images/icon-lizard.svg",
+    wins: ["spock", "paper"],
+    loses: ["scissors", "rock"]
+  }
+]
+
 export default function App() {
   return (
     <>
       <main>
-        Score Rules You Picked The House Picked You Win You Lose Play Again
+        <h1>CLEAR</h1>
       </main>
       <footer className="">
         Challenge by
