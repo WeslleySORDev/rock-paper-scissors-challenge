07eb5de9fc09376375bcfd9c3ee2feef642f1249
WeslleySORDev <weslleysordev@gmail.com>
2025-03-15 09:19:33 -0300

    feat: add typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 
-createRoot(document.getElementById("root")).render(
+const mainDiv = document.getElementById("root")!;
+createRoot(mainDiv).render(
   <StrictMode>
     <App />
   </StrictMode>
